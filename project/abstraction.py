e7af991602237ad7d375f691f466779e3dbfed2d
magladko <kornel.marek@gmail.com>
2025-10-06 16:59:56 +0200

    add abstraction

--- project/abstraction.py
+++ project/abstraction.py
@@ -18,6 +18,15 @@ class SignSet:
             signset.add("-")
         return cls(signset)
 
+    def compare_signs(self, s1: Sign, s2: Sign) -> set[Sign]:
+        if s1 == s2:
+            return {s1}
+        if s1 == "0" and s2 != "0":
+            return {s1}
+        if s1 != "0" and s2 == "0":
+            return {s2}
+        return {"+", "-", "0"}
+
     def __contains__(self, member : int):
         if (member == 0 and "0" in self.signs):
             return True
@@ -26,3 +35,19 @@ class SignSet:
         if (member < 0 and "-" in self.signs):
             return True
         return False
+
+    def __add__(self, other):
+        assert isinstance(other, SignSet)
+        new_signlist = SignSet(set())
+        for ss in self.signs:
+            for os in other.signs:
+                new_signlist.signs.update(self.compare_signs(ss, os))
+        return new_signlist
+
+    def __le__(self, other):
+        assert isinstance(other, SignSet)
+        return self.signs <= other.signs
+
+    def __eq__(self, other):
+        assert isinstance(other, SignSet)
+        return self.signs == other.signs
--- project/test/test_abstraction.py
+++ project/test/test_abstraction.py
@@ -1,9 +1,23 @@
 
 from hypothesis import given
 from hypothesis.strategies import integers, sets
-from abstraction import SignSet
+from ..abstraction import SignSet
 
 @given(sets(integers()))
 def test_valid_abstraction(xs):
-  s = SignSet.abstract(xs) 
-  assert all(x in s for x in xs)
+    s = SignSet.abstract(xs) 
+    assert all(x in s for x in xs)
+
+@given(sets(integers()), sets(integers()))
+def test_sign_adds(xs, ys):
+    assert (
+      SignSet.abstract({x + y for x in xs for y in ys}) 
+        <= SignSet.abstract(xs) + SignSet.abstract(ys)
+    )
+    
+# @given(sets(integers()), sets(integers()))
+# def test_sign_adds(xs, ys):
+#     assert (
+#       {x <= y for x in xs for y in ys} 
+#       <= arithmetic.compare("le", SignSet.abstract(xs), SignSet.abstract(ys))
+#     )
